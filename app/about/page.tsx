75ed3ec17066b6b0823f360e880770b5e0bf1545
hlee3088 <hlee3088@gatech.edu>
2025-04-18 14:36:23 -0400

    placeholder

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
 export default function AboutPage() {
-    return <div className="py-20 px-6">About Page</div>;
+    return <div className="py-20 px-6">Working on it</div>;
   }
   
---
+++ app/contact/page.tsx
@@ -0,0 +1,4 @@
+export default function AboutPage() {
+    return <div className="py-20 px-6">Working on it</div>;
+  }
+  
